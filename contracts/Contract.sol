020ca2546d7a5465000d33029e02de92069eb4be
AndreyKomin <andrey.s.komin@gmail.com>
2023-01-19 11:44:43 +0300

    first contract version

--- contracts/Contract.sol
+++ contracts/Contract.sol
@@ -1,6 +1,28 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.9;
 
-contract MyContract {
-    constructor() {}
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+
+contract GameItem is ERC721URIStorage {
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
+
+    constructor(string memory tokenName, string memory symbol) public ERC721("GameItem", "ITM") {
+        _setTokenURI("ipfs://");
+    }
+
+    function mintItem(address player, string memory tokenURI)
+    public
+    returns (uint256)
+    {
+        _tokenIds.increment();
+
+        uint256 newItemId = _tokenIds.current();
+        _mint(player, newItemId);
+        _setTokenURI(newItemId, tokenURI);
+
+        return newItemId;
+    }
 }
